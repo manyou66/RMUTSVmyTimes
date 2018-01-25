817e7cb7e788e3b4ba946c1cb4c451c63fd58bbd
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-25 16:01:52 +0700

    On OFF Switch Success

--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import wanpracha.rmutsv.ac.th.rmutsvmytimes.R;
 
@@ -13,7 +20,64 @@ import wanpracha.rmutsv.ac.th.rmutsvmytimes.R;
  * Created by bow on 25/1/2561.
  */
 
-public class ControllerFragment extends Fragment{
+public class ControllerFragment extends Fragment {
+
+    //    Explicit
+    private Button zeroButton, onButton, offButton;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+        zeroButton = getView().findViewById(R.id.btnZero);
+        onButton = getView().findViewById(R.id.btnOn);
+        offButton = getView().findViewById(R.id.btnOff);
+
+//        Setup Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+
+//        Zero Controller
+
+        zeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String, Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("myTimes", "0");
+                databaseReference.updateChildren(stringObjectMap);
+
+
+            } // onClick
+        });
+//        On Controller
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("Switch", 1);
+                databaseReference.updateChildren(stringObjectMap);
+
+            }
+        });
+
+//        offController
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("Switch", 0);
+                databaseReference.updateChildren(stringObjectMap);
+            }
+        });
+
+
+    }//Main Method
 
     @Nullable
     @Override
