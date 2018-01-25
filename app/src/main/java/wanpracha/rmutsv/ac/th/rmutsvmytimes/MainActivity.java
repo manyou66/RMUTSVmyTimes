bbf7aa346dbca3ee51771b8d8cc792edfbefb66e
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-25 14:22:11 +0700

    Design UI of Controller Success

--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
@@ -3,6 +3,7 @@ package wanpracha.rmutsv.ac.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import wanpracha.rmutsv.ac.th.rmutsvmytimes.fragment.ControllerFragment;
 import wanpracha.rmutsv.ac.th.rmutsvmytimes.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
 //        Add Fragment of Activity
         if (savedInstanceState == null) {
+
+//            For MainFragment
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentMainFragment, new MainFragment())
                     .commit();
-
-
+//            For ControllerFragment
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentControllerFragment, new ControllerFragment())
+                    .commit();
         }
 
 
