f74e84b6367d3c173f36a0bf0cd7efa0a63344a3
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-25 11:43:40 +0700

    Add fragment success

--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
@@ -3,11 +3,27 @@ package wanpracha.rmutsv.ac.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import wanpracha.rmutsv.ac.th.rmutsvmytimes.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //Keep status app
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment of Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+
+        }
+
+
+    }//Main Method
+
+
+
+}//Main Class
