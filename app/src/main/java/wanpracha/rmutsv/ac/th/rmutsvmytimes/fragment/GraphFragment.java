803624c5c1bcea8d5069930d6235f35e3c3ecb31
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-26 13:33:42 +0700

    Show Graph from database success

--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/GraphFragment.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/GraphFragment.java
@@ -3,17 +3,75 @@ package wanpracha.rmutsv.ac.th.rmutsvmytimes.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import wanpracha.rmutsv.ac.th.rmutsvmytimes.R;
+import wanpracha.rmutsv.ac.th.rmutsvmytimes.utility.GetAllData;
 
 /**
  * Created by bow on 26/1/2561.
  */
 
 public class GraphFragment extends Fragment{
+
+
+    //        Explicit
+    private GraphView graphView;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create GraphView
+        try {
+            String strURL = "http://androidthai.in.th/piw/getAllFirebaseBrownie.php";
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(strURL);
+            String strJSON = getAllData.get();
+            Log.d("26Jan", "JSON ==>" + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            int[] xInts = new int[jsonArray.length()];
+            int[] yInts = new int[jsonArray.length()];
+            DataPoint[] dataPoints = new DataPoint[jsonArray.length()];
+
+
+            for (int i=0; i<jsonArray.length();i+=1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String strX = jsonObject.getString("X");
+                String stry = jsonObject.getString("Y");
+                xInts[i] = Integer.parseInt(strX);
+                yInts[i] = Integer.parseInt(stry);
+
+                dataPoints[i] = new DataPoint(xInts[i], yInts[i]);
+
+            }//for
+
+            graphView = getView().findViewById(R.id.graphViewMyData);
+            LineGraphSeries<DataPoint> dataPointLineGraphSeries = new LineGraphSeries<>(dataPoints);
+            graphView.addSeries(dataPointLineGraphSeries);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }//Main Method
 
     @Nullable
     @Override
--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/MainFragment.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/fragment/MainFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +19,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Map;
+import java.util.Random;
 
 import wanpracha.rmutsv.ac.th.rmutsvmytimes.GraphActivity;
 import wanpracha.rmutsv.ac.th.rmutsvmytimes.R;
+import wanpracha.rmutsv.ac.th.rmutsvmytimes.utility.PostData;
 
 /**
  * Created by bow on 25/1/2561.
@@ -52,6 +55,25 @@ public class MainFragment extends Fragment{
                 Map map = (Map) dataSnapshot.getValue();
                 myTimesString = String.valueOf(map.get("myTimes"));
                 textView.setText(myTimesString);
+
+//                Get and Post Data From Firebase to mySQL
+                try {
+                    String strURL = "http://androidthai.in.th/piw/addGraphBrownie.php";
+                    Random random = new Random();
+                    int intX = Integer.parseInt(myTimesString);
+                    int intY = random.nextInt(10) + intX;
+
+                    PostData postData = new PostData(getActivity());
+                    postData.execute(Integer.toString(intX),
+                            Integer.toString(intY),
+                                    strURL);
+                    String strResult = postData.get();
+                    Log.d("26Jan", "Result ==>" +strResult);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
 
             }//onDataChange
