b1f05bc5827e82539add8cb55e6c4441b61ab69a
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-26 09:51:27 +0700

    Add Graph Fragment success

--- app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/GraphActivity.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/rmutsvmytimes/GraphActivity.java
@@ -3,11 +3,26 @@ package wanpracha.rmutsv.ac.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import wanpracha.rmutsv.ac.th.rmutsvmytimes.fragment.GraphFragment;
+
 public class GraphActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-    }
-}
+
+        //    Add Fragment
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentGraphFragment, new GraphFragment())
+                    .commit();
+
+        }
+
+    }//Main Method
+
+
+
+}//Main Class
+
